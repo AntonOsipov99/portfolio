74b9b57934774de58b806fd24d97f0713b8fe63b
AntonOsipov99 <antonosipov@gmx.de>
2024-01-23 23:50:21 +0100

    portfolio finished

--- portfolio/src/app/landing-page/contact/contact.component.ts
+++ portfolio/src/app/landing-page/contact/contact.component.ts
@@ -29,35 +29,70 @@ export class ContactComponent implements OnInit {
 
 
   async sendMail() {
-    let nameField = this.nameField.nativeElement;
-    let emailField = this.emailField.nativeElement;
-    let messageField = this.messageField.nativeElement;
-    let sendButton = this.sendButton.nativeElement;
-    sendButton.innerText = 'Mail sent';
-    sendButton.style.backgroundColor = 'green';
-    nameField.disabled = true;
-    emailField.disabled = true;
-    messageField.disabled = true;
-    sendButton.disabled = true;
-    let fd = new FormData();
-    fd.append('name', nameField.value);
-    fd.append('email', emailField.value);
-    fd.append('message', messageField.value);
-    await fetch('https://anton-osipov.de/send_mail.php',
-      {
-        method: 'post',
-        body: fd
-      })
-    nameField.value = '';
-    emailField.value = '';
-    messageField.value = '';
+    this.updateUIOnSend();
+    const formData = this.createFormData();
+    await this.sendFormData(formData);
+    this.setSuccessUI();
+    this.resetUI();
+  }
+
+  private updateUIOnSend() {
+    const sendButton = this.sendButton.nativeElement;
+    sendButton.innerText = 'Sending';
+    sendButton.style.backgroundColor = 'orange';
+    this.disableFormFields();
+  }
+
+  private createFormData(): FormData {
+    const formData = new FormData();
+    formData.append('name', this.nameField.nativeElement.value);
+    formData.append('email', this.emailField.nativeElement.value);
+    formData.append('message', this.messageField.nativeElement.value);
+    return formData;
+  }
+
+  private async sendFormData(formData: FormData) {
+    await fetch('https://anton-osipov.de/send_mail.php', {
+      method: 'post',
+      body: formData
+    });
+  }
+
+  private resetForm() {
+    this.nameField.nativeElement.value = '';
+    this.emailField.nativeElement.value = '';
+    this.messageField.nativeElement.value = '';
+  }
+
+  private setSuccessUI() {
+    const sendButton = this.sendButton.nativeElement;
     setTimeout(() => {
+      sendButton.innerText = 'Success';
+      sendButton.style.backgroundColor = 'green';
+      this.resetForm();
+    }, 1500);
+  }
+
+  private resetUI() {
+    setTimeout(() => {
+      const sendButton = this.sendButton.nativeElement;
       sendButton.innerText = 'Send message';
       sendButton.style.backgroundColor = '#00BEE8';
-      nameField.disabled = false;
-      emailField.disabled = false;
-      messageField.disabled = false;
-      sendButton.disabled = false;
-    }, 2000);
+      this.enableFormFields();
+    }, 3000);
+  }
+
+  private disableFormFields() {
+    this.nameField.nativeElement.disabled = true;
+    this.emailField.nativeElement.disabled = true;
+    this.messageField.nativeElement.disabled = true;
+    this.sendButton.nativeElement.disabled = true;
+  }
+
+  private enableFormFields() {
+    this.nameField.nativeElement.disabled = false;
+    this.emailField.nativeElement.disabled = false;
+    this.messageField.nativeElement.disabled = false;
+    this.sendButton.nativeElement.disabled = false;
   }
 }
