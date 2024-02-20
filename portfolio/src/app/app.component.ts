9e39e05761f0562989c02b24cb412e4fb5a06a97
AntonOsipov99 <antonosipov@gmx.de>
2024-02-20 17:17:32 +0100

    add animations

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, LandingPageComponent,],
+  imports: [CommonModule, RouterOutlet, LandingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
